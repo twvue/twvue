d81e289ce2e98f2e9468f5d258c0cb70c8d60123
Luis Davi <luisdavi.contato@gmail.com>
2022-11-12 19:17:49 -0300

    update the navigation bar

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,18 +1,14 @@
 <template>
     <div id="app">
-        <div class="mb-10 bg-gray-100">
-            <div class="container mx-auto p-4 space-x-3">
-                <RouterLink
-                    v-for="nav in menu"
-                    :key="nav.label"
-                    :to="nav.to"
-                >
-                    {{ nav.label }}
-                </RouterLink>
+        <div class="">
+            <div class="w-full px-12 py-4 space-x-4 flex flex-row items-center  ">
+                <img src="../src/assets/images/twvue_bgTransparent.png" alt="logo" height="75" width="50">
+                <router-link v-for="(nav, index) in menu" :key="index" :to="nav.to" :class="['p-2 rounded-lg bg-sky-500/50', {'bg-blue-100 text-blue-500': nav.to === $route.matched[0].path || nav.to === $route.fullPath}]">{{nav.label}}</router-link>
             </div>
+            
         </div>
 
-        <div class="container mx-auto p-4">
+        <div class="px-12 mx-auto px-4">
             <RouterView />
         </div>
     </div>
