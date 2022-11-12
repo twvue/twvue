44f7351e75266134d230f59d7c18a6d4d11c59d5
Luis Davi <luisdavi.contato@gmail.com>
2022-11-12 19:10:19 -0300

    update the Home page

--- docs/src/views/Home.vue
+++ docs/src/views/Home.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
-        Welcome!
+    <div class="w-full h-max flex flex-col items-center justify-center gap-4 p-2">
+        <div class="w-full h-max p-4 flex flex-col items-center justify-center gap-8">
+            <img src="../assets/images/twvue_blue.png" 
+            alt="twvue-Logo" height="200" width="200"
+            class="rounded-full " id="logo"/>
+            <p class="text-4xl text-gray-800 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit </p>
+            <div class="flex flex-row gap-4">
+                <router-link to="/documentation">
+                    <TWButton variant="primary" size="xl">
+                        Get started
+                    </TWButton>                
+                </router-link>
+                <a href="https://www.github.com/twvue/twvue" target="_blank" class="-p-2">
+                    <TWButton variant="secondaryOutline" size="xl">
+                        <div class="flex flex-row gap-2 items-center justify-center text-gray-800">
+                            <span class="font-bold">Github</span>
+                            <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 fill-slate-900"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path></svg>
+                        </div>
+                    </TWButton>
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,6 +33,19 @@
         },
 
         methods: {
+            
         },
     };
 </script>
+<style>
+@keyframes logoJump{
+    0%{
+        transform: translateY(-12px);
+    }50%{
+        transform: translateY(12px);
+    }100%{
+        transform: translateY(-12px);
+    }
+}
+
+</style>
