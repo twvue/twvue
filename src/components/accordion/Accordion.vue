f2dcc68057d1aec1b584c2e005b60d6afa47d390
Luis Davi <luisdavi.contato@gmail.com>
2022-11-14 20:57:04 -0300

    added shadow in component body & border top in content

--- src/components/accordion/Accordion.vue
+++ src/components/accordion/Accordion.vue
@@ -1,15 +1,29 @@
 <template>
     <div :class="rootClass">
-        <div @click="checkClickToToggleContent()" :class="[headerClass, tittleClass]">
-            <slot name="tittle"/>
+        <div
+            @click="checkClickToToggleContent()"
+            :class="[headerClass, tittleClass]"
+        >
+            <slot name="tittle" />
             <div class="">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" :class="iconClass">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    :class="iconClass"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    />
                 </svg>
             </div>
         </div>
         <div :class="[contentClass, toggleContentClass]">
-            <slot name="content" :selected="selected"/>
+            <slot name="content" :selected="selected" />
         </div>
     </div>
 </template>
@@ -18,39 +32,44 @@
 import FixedMixin from "../../utils/FixedMixin";
 import VariantMixin from "../../utils/VariantMixin";
 import SizeMixin from "../../utils/SizeMixin";
-    export default {
-        data() {
-            return {
-                config: this.$TWVue.Accordion,
-                selected: false,
-            }
+export default {
+    data() {
+        return {
+            config: this.$TWVue.Accordion,
+            selected: false
+        };
+    },
+    mixins: [FixedMixin, VariantMixin, SizeMixin],
+    methods: {
+        checkClickToToggleContent() {
+            this.selected = this.selected == true ? false : true;
+        }
+    },
+    computed: {
+        rootClass() {
+            return [
+                this.fixedClass.root,
+                this.variantClass.root,
+                this.sizeClass.root
+            ];
         },
-        mixins: [FixedMixin, VariantMixin, SizeMixin],
-        methods: {
-          checkClickToToggleContent(){
-            this.selected = (this.selected == true)? false : true
-          }  
+        headerClass() {
+            return [this.fixedClass.header, this.variantClass.header];
         },
-        computed:{
-            rootClass(){
-                return [this.fixedClass.root, this.variantClass.root, this.sizeClass.root]
-            },
-            headerClass(){
-                return[this.fixedClass.header, this.variantClass.header]
-            },
-            tittleClass(){
-                return [this.fixedClass.tittle, this.variantClass.tittle]
-            },
-            iconClass(){
-                return [this.fixedClass.icon, this.variantClass.icon]
-            },
-            contentClass(){
-                return [this.fixedClass.content.fixed]
-            },
-            toggleContentClass(){
-                return  {[this.fixedClass.content.actived]: this.selected == true, [this.fixedClass.content.noActived]: this.selected == false}
-            },
-
+        tittleClass() {
+            return [this.fixedClass.tittle, this.variantClass.tittle];
         },
+        iconClass() {
+            return [this.fixedClass.icon, this.variantClass.icon];
+        },
+        contentClass() {
+            return [this.fixedClass.content.fixed];
+        },
+        toggleContentClass() {
+            return this.selected
+                ? [this.fixedClass.content.actived]
+                : this.fixedClass.content.noActived;
+        }
     }
+};
 </script>
--- src/components/accordion/config.js
+++ src/components/accordion/config.js
@@ -2,50 +2,50 @@ export default {
     defaultVariant: "primary",
     defaultSize: "md",
     fixed: {
-        root: "bg-white rounded-lg border-2 overflow-hidden",
-        header: "p-2 flex justify-between items-center cursor-pointer ",
+        root: "bg-white shadow-lg rounded-lg border-2 overflow-hidden flex flex-col",
+        header: "p-2 flex justify-between items-center cursor-pointer border-b",
         tittle: 'text-xl',
         icon: 'w-6 h-6',
         content: {
-            fixed: 'transition-all duration-200 px-2 ',
-            actived: 'h-max p-2 border-t border-gray-200',
+            fixed: 'transition-all duration-200',
+            actived: 'h-max p-2',
             noActived: 'max-h-0'
         }
     },
     variants: {
         primary: {
-            root: "border-blue-500",
-            header: 'bg-blue-500',
+            root: "border-blue-500 ",
+            header: 'bg-blue-500 border-transparent',
             tittle: 'text-white',
             icon: "text-white"
         },
         secondary: {
             root: "border-gray-500",
-            header: 'bg-gray-500',
+            header: 'bg-gray-500 border-transparent',
             tittle: 'text-white',
             icon: "text-white"
         },
         success: {
             root: "border-green-500",
-            header: 'bg-green-500',
+            header: 'bg-green-500 border-transparent',
             tittle: 'text-white',
             icon: "text-white"
         },
         danger: {
             root: "border-red-500",
-            header: 'bg-red-500',
+            header: 'bg-red-500 border-transparent',
             tittle: 'text-white',
             icon: "text-white"
         },
         warning: {
             root: "border-yellow-500",
-            header: 'bg-yellow-500',
+            header: 'bg-yellow-500 border-transparent',
             tittle: 'text-white',
             icon: "text-white"
         },
         ghost:{
             root: "border-gray-200 text-black",
-            header: 'bg-transparent',
+            header: 'bg-transparent border-gray-200',
             tittle: 'text-black',
             icon: "text-black"
         }
